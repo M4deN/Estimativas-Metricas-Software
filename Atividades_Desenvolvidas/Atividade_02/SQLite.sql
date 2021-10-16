eba59d88118746793a84e18eb5d333b6daad2721
Alécio L. Medeiros <charlie_brandbury@outlook.com>
2021-10-16 08:09:24 -0300

    Create SQLite.sql

---
+++ Atividades_Desenvolvidas/Atividade_02/SQLite.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Clientes(
+  nome varchar(50)
+  rua VARCHAR(50)
+  numero INT
+  cidade VARCHAR(10)
+  uf(VARCHAR(20)
+  cep VARCHAR(20)
+  pais(VARCHAR(30)
+  cpf VARCHAR(20)
+  rg VARCHAR(20)
+  email VARCHAR(30)
+  homepage VARCHAR(50)
+  telefone VARCHAR(20)
+  celular VARCHAR(20)
+  fax VARCHAR(20)
+  dataNasc DATE
+       )
+  
